94156b8b6a609cd36acc8ff3a30dbd34b3cd1231
rizkysyafrian <rzkyndr@gmail.com>
2021-07-19 18:24:44 +0700

    pipeline test 4

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 const port = 8080;
 
-app.get('/', (req, res) => res.send(index.html));
+app.get('/', (req, res) => res.redirect("/index.html"));
 
 app.listen(port);
 console.log(`Test Repo App running on http://localhost:${port}`);
