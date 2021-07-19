19685f2104d8e6890a8eeecb2e4cae66de7494c6
rizkysyafrian <rzkyndr@gmail.com>
2021-07-19 21:08:30 +0700

    1

--- app.js
+++ app.js
@@ -1,10 +1,22 @@
-const express = require('express');
-const app = express();
-const port = 8080;
+// const express = require('express');
+// const app = express();
+// const port = 8080;
 
-app.get('/', (req, res) => res.send("Hello World!"));
+// app.get('/', (req, res) => res.send("Hello World!"));
 
-//res.write("Hello World! 2");
+// //res.write("Hello World! 2");
 
-app.listen(port);
-console.log(`Test Repo App running on http://localhost:${port}`);
+// app.listen(port);
+
+//const Centrifuge = require('centrifuge');
+//const WebSocket = require('ws');
+
+var http = require('http');
+
+http.createServer(function (req, res) {
+  res.writeHead(200, {'Content-Type': 'text/html'});
+  res.write('Hello World!');
+  res.end();
+}).listen(8080);
+
+console.log("Test Repo App running on http://localhost:");
