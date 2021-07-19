ad54f843a6b4847e2fb4a38124334bc51bf55b5c
rizkysyafrian <rzkyndr@gmail.com>
2021-07-19 20:31:44 +0700

    delete index

--- app.js
+++ app.js
@@ -4,5 +4,7 @@ const port = 8080;
 
 app.get('/', (req, res) => res.send("Hello World!"));
 
+res.write("Hello World! 2");
+
 app.listen(port);
 console.log(`Test Repo App running on http://localhost:${port}`);
